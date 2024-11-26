02059a55c0a16980bd586ac84842d82816b24f7a
Berat Sakipi <berat.sakipi@gmail.com>
2024-11-26 19:22:10 +0100

    Made the app more accessible by putting it into a main tag, adding aria-label to the button and aria-live to the h2 that changes dynamically. Also made so that the text-color of the activity changes to make it more visible on change

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const handleGenerateBtnClick = () => {
     fetch("https://apis.scrimba.com/bored/api/activity")
       .then(response => response.json())
       .then(data => {
-          console.log(data)
-          document.getElementById("activity-name").textContent = data.activity
+          const activity = document.querySelector('#activity-name');
+          activity.textContent = data.activity
+          activity.classList.add('activity-name__fun')
       })
   }
   const generateActivityBtn = document.querySelector('#generate-activity');
