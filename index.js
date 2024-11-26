e1808e0be96f0319216d744e775757c5802c6a22
Berat Sakipi <berat.sakipi@gmail.com>
2024-11-26 20:55:44 +0100

    Consistency changes, the semicolon

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const handleGenerateBtnClick = () => {
     fetch("https://apis.scrimba.com/bored/api/activity")
       .then(response => response.json())
       .then(data => {
-          const activity = document.querySelector('#activity-name');
+          const activity = document.querySelector('#activity-name')
           activity.textContent = data.activity
           activity.classList.add('activity-name__fun')
       })
   }
-  const generateActivityBtn = document.querySelector('#generate-activity');
+  const generateActivityBtn = document.querySelector('#generate-activity')
   generateActivityBtn.addEventListener('click', handleGenerateBtnClick)
